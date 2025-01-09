185fbfda153ca295d11916f4344ebce020e2249a
AbooMinister25 <aboominister@gmail.com>
2025-01-09 15:42:42 -0500

    Fix template

--- flake.nix
+++ flake.nix
@@ -6,32 +6,33 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs ={ self, nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem
-    (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-        };
-      in
-      with pkgs;
-      {
-        devShells.default = (pkgs.buildFHSUserEnv {
-          name = "uv";
-          targetPkgs = pkgs: (
-            with pkgs;[
-              python312
-              uv
-            ]
-          );
-          runScript = "fish";
-        }).env;
-
-        templates = {
-          default = {
-            path = ./.;
+  outputs ={ self, nixpkgs, flake-utils, ... }: {
+    devShells = flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
           };
-        };
-      }
-    );
+        in
+        with pkgs;
+        {
+          devShells.default = (pkgs.buildFHSUserEnv {
+            name = "uv";
+            targetPkgs = pkgs: (
+              with pkgs;[
+                python312
+                uv
+              ]
+            );
+            runScript = "fish";
+          }).env;
+        }
+      );
+
+    templates = {
+      default = {
+        path = ./.;
+      };
+    };
+  };
 }
